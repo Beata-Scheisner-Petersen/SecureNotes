ea6b6d388ac43354fc2a6789d9ca5b806d9eac4b
soppitopp <soppitopp2000@hotmail.com>
2026-04-18 20:12:53 +0200

    add: mySqlConnectionFactory.java

---
+++ src/main/java/securenotes/repository/mysql/mySqlConnectionFactory.java
@@ -0,0 +1,27 @@
+package securenotes.repository.mysql;
+
+import securenotes.logging.ErrorLogger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class mySqlConnectionFactory {
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+
+    public static Connection getSqlConnection () {
+        RuntimeException runtimeException = new RuntimeException();
+        if (URL.isBlank() || USER.isBlank() || PASSWORD.isBlank()) {
+            ErrorLogger.log("Missing required environment variables", runtimeException);
+        }
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException sqlException) {
+            ErrorLogger.log("failed to connect with the database", sqlException);
+        }
+
+        return connection;
+    }
+}
