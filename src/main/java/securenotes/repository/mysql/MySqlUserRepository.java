bd639ed0be1e72fd08abf1b058ae96d27bd68fd0
soppitopp <soppitopp2000@hotmail.com>
2026-04-18 20:13:10 +0200

    add: MySqlUserRepository.java

---
+++ src/main/java/securenotes/repository/mysql/MySqlUserRepository.java
@@ -0,0 +1,77 @@
+package securenotes.repository.mysql;
+
+import org.mindrot.jbcrypt.BCrypt;
+import securenotes.logging.ErrorLogger;
+import securenotes.model.User;
+import securenotes.repository.interfaces.IUserRepository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class MySqlUserRepository implements IUserRepository {
+
+    @Override
+    public User findById(int id) {
+        //noinspection SqlResolve
+        String sql = "SELECT id, username, password FROM users WHERE id = ?";
+
+        try(Connection connection = mySqlConnectionFactory.getSqlConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                return new User(
+                        result.getInt("id"),
+                        result.getString("username"),
+                        result.getString("password")
+                );
+            }
+        } catch (SQLException e) {
+            ErrorLogger.log("failed to find user by id", e);
+        }
+        return null;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        //noinspection SqlResolve
+        String sql = "SELECT id, username, password FROM users WHERE username = ?";
+
+        try(Connection connection = mySqlConnectionFactory.getSqlConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, username);
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                return new User(
+                        result.getInt("id"),
+                        result.getString("username"),
+                        result.getString("password")
+                );
+            }
+        } catch (SQLException e) {
+            ErrorLogger.log("failed to find user by id", e);
+        }
+        return null;
+    }
+
+    @Override
+    public void save(User user) {
+        //noinspection SqlResolve
+        String insert = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+
+        try (Connection connection = mySqlConnectionFactory.getSqlConnection();
+             PreparedStatement statement = connection.prepareStatement(insert)) {
+            statement.setString(1, user.getUsername());
+            statement.setString(2, hashedPassword);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error: sql injection failed");
+            ErrorLogger.log("failed to save user", e);
+        }
+    }
+}
