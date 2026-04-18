b335ed188b01ca6d8a67e891bde85c53b1650226
soppitopp <soppitopp2000@hotmail.com>
2026-04-18 19:34:57 +0200

    add: INoteRepository.java

---
+++ src/main/java/securenotes/repository/interfaces/INoteRepository.java
@@ -0,0 +1,12 @@
+package securenotes.repository.interfaces;
+
+import securenotes.model.Note;
+
+import java.util.List;
+public interface INoteRepository {
+    List<Note> NotesList(int userId);
+    Note findById(int id);
+    void save(Note note);
+    void update(Note note);
+    void delete(int id);
+}
