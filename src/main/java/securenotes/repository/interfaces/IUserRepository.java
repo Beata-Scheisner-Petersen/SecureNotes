c0cdef9f6e6ad035e1d8c74f03eca99a361f5c08
soppitopp <soppitopp2000@hotmail.com>
2026-04-18 19:32:00 +0200

    add: IUserRepository.java

---
+++ src/main/java/securenotes/repository/interfaces/IUserRepository.java
@@ -0,0 +1,8 @@
+package securenotes.repository.interfaces;
+
+import securenotes.model.User;
+public interface IUserRepository {
+    User findById(int id);
+    User findByUsername(String username);
+    void save(User user);
+}
