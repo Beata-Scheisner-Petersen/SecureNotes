878e27a93323fba673df1f9fe3eee817a58e6b32
soppitopp <soppitopp2000@hotmail.com>
2026-04-19 14:07:14 +0200

    Add: imports and functions registerNewUser and validate username and password

--- src/main/java/securenotes/service/AuthService.java
+++ src/main/java/securenotes/service/AuthService.java
@@ -4,9 +4,16 @@ import org.mindrot.jbcrypt.BCrypt;
 import securenotes.logging.ErrorLogger;
 import securenotes.model.AuthSession;
 import securenotes.model.User;
+import securenotes.repository.mysql.mySqlBannedWordRepository;
 import securenotes.repository.mysql.mySqlUserRepository;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AuthService {
     private final mySqlUserRepository userRepository;
+    private final mySqlBannedWordRepository bannedWordRepository = new mySqlBannedWordRepository();
+    private final List<String> blacklistedContent = bannedWordRepository.getBannedWords();
 
     public AuthService(mySqlUserRepository userRepository) {
         this.userRepository = userRepository;
@@ -42,10 +49,54 @@ public class AuthService {
         if (userRepository.findByUsername(username) != null) {
             System.out.println("User already exist");
             return false;
+        } else if ((username.length() < 5 || username.length() > 15 || !isValidUsername(username))) {
+            System.out.println("Error: invalid choice of username");
+            return false;
+        } else if ((password.length() < 8 || password.length() > 15) || !isValidPassword(password)) {
+            System.out.println("Error: invalid choice of password");
+            return false;
+        } else {
+            User user = new User(username, hashedPassword(password));
+            userRepository.save(user);
+            return true;
+        }
+    }
+
+    private boolean isValidUsername(String username) {
+        Pattern specialCharacters = Pattern.compile("[^a-z0-9_]", Pattern.CASE_INSENSITIVE);
+        Matcher matcherForUsername = specialCharacters.matcher(username);
+
+        if (username.isBlank() || matcherForUsername.find() || isBannedWord(username)) {
+            return false;
         }
 
-        User user = new User(username, hashedPassword(password));
-        userRepository.save(user);
+        return true;
+    }
+
+    private boolean isBannedWord(String input) {
+        for (String word : blacklistedContent) {
+            if (input.contains(word) ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isValidPassword(String password) {
+        Pattern specialCharacters = Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
+        Pattern uppercaseCharacters = Pattern.compile("[A-Z]");
+        Pattern lowercaseCharacters = Pattern.compile("[a-z]");
+        Pattern numberCharacter = Pattern.compile("[0-9]");
+
+        Matcher matcherForPassword = specialCharacters.matcher(password);
+        Matcher matcherCheckForUppercase = uppercaseCharacters.matcher(password);
+        Matcher matcherCheckForLowercase = lowercaseCharacters.matcher(password);
+        Matcher matcherCheckForNumbers = numberCharacter.matcher(password);
+
+        if (password.isBlank() || !matcherForPassword.find() || !matcherCheckForUppercase.find() || !matcherCheckForLowercase.find() ||
+                !matcherCheckForNumbers.find() || isBannedWord(password)) {
+            return false;
+        }
         return true;
     }
 }
