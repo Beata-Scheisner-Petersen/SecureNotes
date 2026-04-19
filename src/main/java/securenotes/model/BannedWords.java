e570ad257b448e71630cf374ea58f8fd69abd706
soppitopp <soppitopp2000@hotmail.com>
2026-04-19 13:15:42 +0200

    Add: BannedWords.java

---
+++ src/main/java/securenotes/model/BannedWords.java
@@ -0,0 +1,13 @@
+package securenotes.model;
+
+public class BannedWords {
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
