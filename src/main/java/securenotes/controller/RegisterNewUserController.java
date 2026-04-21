7d7023749231bc202272c15dc3374083d67c03cf
soppitopp <soppitopp2000@hotmail.com>
2026-04-21 19:12:39 +0200

    Add: RegisterNewUserController.java

---
+++ src/main/java/securenotes/controller/RegisterNewUserController.java
@@ -0,0 +1,35 @@
+package securenotes.controller;
+
+import securenotes.logging.ErrorLogger;
+import securenotes.service.AuthService;
+import securenotes.service.FormatDateTimeService;
+import securenotes.view.RegisterNewUserView;
+
+import java.util.Scanner;
+public class RegisterNewUserController {
+    private final AuthService service;
+    private final RegisterNewUserView register;
+    private FormatDateTimeService formatDateTimeService;
+
+    public RegisterNewUserController(AuthService service, RegisterNewUserView register) {
+        this.service = service;
+        this.register = register;
+    }
+
+
+    public void start(Scanner readInput) {
+        Exception exception = new Exception();
+        register.show();
+
+        String username = register.getUsernameInput(readInput);
+        String password = register.getPasswordInput(readInput);
+
+        if (service.registerNewUser(username, password)) {
+            ErrorLogger.log(String.format("[%s] success to create user: %s", formatDateTimeService.FormatDateTime(), username), exception);
+            register.success(username);
+        } else {
+            ErrorLogger.log(String.format("[%s] fail to create user: %s", formatDateTimeService.FormatDateTime(), username), exception);
+            register.error(username);
+        }
+    }
+}
