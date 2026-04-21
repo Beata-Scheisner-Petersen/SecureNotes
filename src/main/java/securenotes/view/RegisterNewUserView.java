312adb943e7b0d07e2f0526e05b182f662429631
soppitopp <soppitopp2000@hotmail.com>
2026-04-21 19:12:54 +0200

    Add: RegisterNewUserView.java

---
+++ src/main/java/securenotes/view/RegisterNewUserView.java
@@ -0,0 +1,33 @@
+package securenotes.view;
+
+import java.util.Scanner;
+public class RegisterNewUserView {
+    private final GetUsernameAndPasswordView credentials = new GetUsernameAndPasswordView();
+    private final ConfirmationView confirmation = new ConfirmationView();
+
+    public void show () {
+        System.out.println("=== Register new user ==");
+    }
+
+    public String getUsernameInput(Scanner readInput) {
+        return credentials.askForUsername(readInput);
+    }
+
+    public String getPasswordInput(Scanner readInput) {
+        return credentials.askForPassword(readInput);
+    }
+
+    public void success(String username) {
+        confirmation.showSuccess(String.format("""
+                        You success to create a new user with username: %s
+                        ------------------------------------------------------------------
+                        """, username));
+    }
+
+    public void error(String username) {
+        confirmation.showError(String.format("""
+                        You failed to create a new user with username: %s
+                        ------------------------------------------------------------------
+                        """, username));
+    }
+}
