f461c6ddf3c4c33f6953dcf2055ff5c7cb2e4a6f
soppitopp <soppitopp2000@hotmail.com>
2026-04-21 19:10:01 +0200

    Add: MainMenuController.java

---
+++ src/main/java/securenotes/controller/MainMenuController.java
@@ -0,0 +1,30 @@
+package securenotes.controller;
+
+import securenotes.view.MainMenuView;
+
+import java.util.Scanner;
+public class MainMenuController {
+    private final MainMenuView view;
+    private final LoginController loginController;
+    private final RegisterNewUserController registerController;
+
+    public MainMenuController(MainMenuView view, LoginController loginController, RegisterNewUserController registerController) {
+        this.view = view;
+        this.loginController = loginController;
+        this.registerController = registerController;
+    }
+
+    public void start(Scanner readInput) {
+        view.show();
+        String choice = view.getChoice(readInput);
+
+        switch (choice) {
+            case "1" -> loginController.start(readInput);
+
+            default -> {
+                System.out.println("Invalid choice, try again!");
+                start(readInput);
+            }
+        }
+    }
+}
