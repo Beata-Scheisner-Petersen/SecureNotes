e0fb1172d657861c84fc052669c24490904ba6f1
soppitopp <soppitopp2000@hotmail.com>
2026-04-21 16:14:27 +0200

    fix: Variable type error

--- src/main/java/securenotes/service/AuthService.java
+++ src/main/java/securenotes/service/AuthService.java
@@ -11,7 +11,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AuthService {
-    private final mySqlUserRepository userRepository;
+    private final IUserRepository userRepository;
     private final mySqlBannedWordRepository bannedWordRepository = new mySqlBannedWordRepository();
     private final List<String> blacklistedContent = bannedWordRepository.getBannedWords();
 
