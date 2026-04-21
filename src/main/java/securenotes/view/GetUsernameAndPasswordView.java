21e01007a4ba988a0ae6d3e8e3c96be55e7285f2
soppitopp <soppitopp2000@hotmail.com>
2026-04-21 19:06:29 +0200

    Add: GetUsernameAndPasswordView.java

---
+++ src/main/java/securenotes/view/GetUsernameAndPasswordView.java
@@ -0,0 +1,16 @@
+package securenotes.view;
+
+import java.util.Scanner;
+
+public class GetUsernameAndPasswordView {
+
+    public String askForUsername(Scanner readInput) {
+        System.out.print("Username: ");
+        return readInput.nextLine();
+    }
+
+    public String askForPassword(Scanner readInput) {
+        System.out.print("Password: ");
+        return readInput.nextLine();
+    }
+}
