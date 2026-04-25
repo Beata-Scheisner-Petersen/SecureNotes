45c23cd597ef8593f992c6840e24fa962ca907c1
soppitopp <soppitopp2000@hotmail.com>
2026-04-25 14:29:14 +0200

    add space

--- src/main/java/securenotes/repository/interfaces/IUserRepository.java
+++ src/main/java/securenotes/repository/interfaces/IUserRepository.java
@@ -1,6 +1,7 @@
 package securenotes.repository.interfaces;
 
 import securenotes.model.User;
+
 public interface IUserRepository {
     User findById(int id);
     User findByUsername(String username);
