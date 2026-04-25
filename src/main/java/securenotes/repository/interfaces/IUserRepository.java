d0aa8405588ade5c3cbc6bc3db0f6329782ebc20
soppitopp <soppitopp2000@hotmail.com>
2026-04-25 14:29:32 +0200

    removed unused method

--- src/main/java/securenotes/repository/interfaces/IUserRepository.java
+++ src/main/java/securenotes/repository/interfaces/IUserRepository.java
@@ -3,7 +3,6 @@ package securenotes.repository.interfaces;
 import securenotes.model.User;
 
 public interface IUserRepository {
-    User findById(int id);
     User findByUsername(String username);
     void save(User user);
 }
