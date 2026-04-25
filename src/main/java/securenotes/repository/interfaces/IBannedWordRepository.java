a5c4fa76b618705da3ec1b5bb1912a9d13d1512a
soppitopp <soppitopp2000@hotmail.com>
2026-04-25 14:28:27 +0200

    add space

--- src/main/java/securenotes/repository/interfaces/IBannedWordRepository.java
+++ src/main/java/securenotes/repository/interfaces/IBannedWordRepository.java
@@ -1,6 +1,7 @@
 package securenotes.repository.interfaces;
 
 import java.util.List;
+
 public interface IBannedWordRepository {
     List<String> getBannedWords ();
     Boolean addWord(String word);
