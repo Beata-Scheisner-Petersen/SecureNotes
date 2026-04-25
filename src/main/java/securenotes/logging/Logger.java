f3fce0ef2a44c79e6be4e0bc63f7b9929856098f
soppitopp <soppitopp2000@hotmail.com>
2026-04-25 14:38:18 +0200

    add space

--- src/main/java/securenotes/logging/Logger.java
+++ src/main/java/securenotes/logging/Logger.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
 public class Logger {
     private static final String LOG_FILE = "src/main/resources/logs/error.log";
 
