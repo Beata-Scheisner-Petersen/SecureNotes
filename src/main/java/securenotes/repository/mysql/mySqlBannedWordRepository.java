c8f6dab54e9708e998bbf842fe8b98850bef3f26
soppitopp <soppitopp2000@hotmail.com>
2026-04-25 14:30:04 +0200

    removed space

--- src/main/java/securenotes/repository/mysql/mySqlBannedWordRepository.java
+++ src/main/java/securenotes/repository/mysql/mySqlBannedWordRepository.java
@@ -2,7 +2,6 @@ package securenotes.repository.mysql;
 
 import securenotes.logging.Logger;
 import securenotes.repository.interfaces.IBannedWordRepository;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
