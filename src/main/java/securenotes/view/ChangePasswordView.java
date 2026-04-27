c40d711ddf70ed2c07a2cd45f000f4c30b5c330d
soppitopp <soppitopp2000@hotmail.com>
2026-04-27 11:28:24 +0200

    add ChangePasswordView.java

---
+++ src/main/java/securenotes/view/ChangePasswordView.java
@@ -0,0 +1,27 @@
+package securenotes.view;
+
+import java.util.Scanner;
+public class ChangePasswordView {
+    private final GetUsernameAndPasswordView credentials = new GetUsernameAndPasswordView();
+    private final Confirmation confirmation = new Confirmation();
+
+    public void show() {
+        System.out.println("=== Change password ===");
+    }
+
+    public String getPasswordInput(Scanner readInput) {
+        return credentials.askForPassword(readInput).trim();
+    }
+    public String getNewPasswordInput(Scanner readInput) {
+        return credentials.askForNewPassword(readInput).trim();
+    }
+
+
+    public void success(String username) {
+        confirmation.showSuccess("Password change");
+    }
+
+    public void error() {
+        confirmation.showError("Password failed to change");
+    }
+}
